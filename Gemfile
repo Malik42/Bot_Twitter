b355df0afccbb946aa91e0b85e15ace25378bc01
Malik42 <malik.ahres@hotmail.fr>
2019-01-16 16:46:25 +0100

    API twitter connected

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source "https://rubygems.org"
 ruby '2.5.1'
 gem 'rspec'
 gem 'dotenv'
+gem 'twitter'
 gem 'pry'
 gem 'rubocop', '~> 0.57.2'
--- lib/bot_twitter.rb
+++ lib/bot_twitter.rb
@@ -1,5 +1,24 @@
 require 'dotenv'
+require 'twitter'
 
-Dotenv.load
+def login()
+  Dotenv.load
+  # quelques lignes qui appellent les clés d'API de ton fichier .env
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
+  
+  if client.nil?
+    return false
+  else
+    return client
+  end
+end
 
-puts ENV['BEST_WEBSITE_EVER']
+user = login
+
+# ligne qui permet de tweeter sur ton compte
+user.update("#bonjour_monde https://www.youtube.com/watch?v=MJTOtrGKEls")
